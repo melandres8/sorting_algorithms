b1892ee792b67c3d0975cba61ca7b363179b9889
Andmontc <oscarandresmontes@gmail.com>
2020-06-14 23:14:23 +0000

    Header and prototypes

--- sort.h
+++ sort.h
@@ -28,5 +28,9 @@ void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap(int *a, int *b);
+void quick_sort(int *array, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
+int partition(int *array, int start, int end, size_t size);
 
 #endif
