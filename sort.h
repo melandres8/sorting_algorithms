c989450e477d92aa4c8a6fab18056c13deee9e34
melandres8 <melanmosta@gmail.com>
2020-06-17 21:46:46 +0000

    update

--- sort.h
+++ sort.h
@@ -33,5 +33,6 @@ void quick_sort(int *array, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
