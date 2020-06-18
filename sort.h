abe6bd5552178565c5ad3a5458e2e17a0e408d02
Andmontc <oscarandresmontes@gmail.com>
2020-06-18 02:24:43 +0000

    Cocktail sort

--- sort.h
+++ sort.h
@@ -34,5 +34,6 @@ void quicksort(int *array, int low, int high, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void shell_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
